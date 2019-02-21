dc1956b3900e01c94999f56da3f9146d73b5a3b2
Whitny Edwards <wne4e2@gmail.com>
2019-02-21 02:39:55 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,12 +29,14 @@ def can_be_created_in_a_block
   end
 end
 
+
+
 def can_get_the_first_item_in_the_database
-  Movie.first.title
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last.title
+  Movie.last
 end
 
 def can_get_size_of_the_database
@@ -42,7 +44,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.all.order(id: :desc).last.title
+  Movie.all.order(id: :desc).last
 end
 
 def can_find_by_multiple_attributes
@@ -93,3 +95,4 @@ def can_destroy_all_items_at_once
   end
   Movie.destroy_all
 end
+
