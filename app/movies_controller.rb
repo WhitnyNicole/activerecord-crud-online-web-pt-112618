43eca13410856a9a753a37f3881b06f0ddcce3e4
Whitny Edwards <wne4e2@gmail.com>
2019-02-21 02:22:47 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,11 +30,11 @@ def can_be_created_in_a_block
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.first.titleize
+  Movie.first.title
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last.titleize
+  Movie.last.title
 end
 
 def can_get_size_of_the_database
